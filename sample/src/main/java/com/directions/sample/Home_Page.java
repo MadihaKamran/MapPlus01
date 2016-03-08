c2ea687ff5f1525b48a66cdd1a45dffabcb8556f
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2016-03-07 23:09:06 -0500

    worked

--- sample/src/main/java/com/directions/sample/Home_Page.java
+++ sample/src/main/java/com/directions/sample/Home_Page.java
@@ -1,11 +1,14 @@
 package com.directions.sample;
-
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
+
+import com.directions.sample.MainActivity;
 
 public class Home_Page extends AppCompatActivity {
 
@@ -13,17 +16,11 @@ public class Home_Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void onClick(View v){
+        Intent i=new Intent(this,MainActivity.class);
+        startActivity(i);
     }
 
 }
